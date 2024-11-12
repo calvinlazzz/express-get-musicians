ed7a17b59316ecf432b2f21344040d9e0d112463
Calvin La <calvin.la@clover.com>
2024-11-12 15:10:52 -0800

    adding tests for crud endpoints

--- index.test.js
+++ index.test.js
@@ -7,7 +7,7 @@ const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+//const {seedMusician} = require("./seedData");
 
 
 describe('./musicians endpoint', () => {
@@ -18,9 +18,42 @@ test('Testing the /musicians endpoint', async () => {
     expect(response.statusCode).toBe(200);
     expect(responseData.length).toBe(3);
 })
-    
-    
-
+test(' Testing the /musicians/:id endpoint', async () => {
+    const response = await request(app).get('/musicians/1');
+    const responseData = JSON.parse(response.text);
+    expect(response.statusCode).toBe(200);
+    expect(responseData.id).toBe(1);
+})
+test(' Testing the /musicians POST endpoint', async () => {
+    const response = await request(app)
+    .post('/musicians')
+    .send({
+        name: 'Calvin',
+        instrument: 'Guitar'
+    });
+    const responseData = JSON.parse(response.text);
+    expect(response.statusCode).toBe(200);
+    expect(responseData.name).toBe('Calvin');
+    expect(responseData.instrument).toBe('Guitar');
+})
+test(' Testing the /musicians PUT endpoint', async () => {
+    const response = await request(app)
+    .put('/musicians/1')
+    .send({
+        name: 'Calvin',
+        instrument: 'Guitar'
+    });
+    const responseData = JSON.parse(response.text);
+    expect(response.statusCode).toBe(200);
+    expect(responseData.name).toBe('Calvin');
+    expect(responseData.instrument).toBe('Guitar');
+})
+test(' Testing the /musicians DELETE endpoint', async () => {
+    const response = await request(app).delete('/musicians/1');
+    const responseData = JSON.parse(response.text);
+    expect(response.statusCode).toBe(200);
+    expect(responseData.message).toBe('Musician 1 deleted');
+})
 
 
 
--- src/app.js
+++ src/app.js
@@ -11,7 +11,27 @@ app.get("/musicians", async (req, res) => {
     res.json(musicians);
 });
 
+app.get("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    res.json(musician);
+})
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.post("/musicians", async (req, res) => {
+    const musician = await Musician.create(req.body);
+    res.json(musician);
+})
+app.put("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    await musician.update(req.body);
+    res.json(musician);
+})
+app.delete("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    await musician.destroy();
+    res.json({ message: `Musician ${req.params.id} deleted` });
+})
 
 
 
