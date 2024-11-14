f2f869411e2369f340cb366b6b82434d05473938
Calvin La <calvin.la@clover.com>
2024-11-14 14:24:23 -0800

    Completed tests for express router

--- index.test.js
+++ index.test.js
@@ -1,61 +1,74 @@
-// install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+
 
 const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
 const app = require('./src/app');
-//const {seedMusician} = require("./seedData");
+const syncSeed = require('./seed.js');
+const router = require('./routes/musicians');
 
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-test('Testing the /musicians endpoint', async () => {
+
+
+
+beforeAll(async () => {
+
+    //await db.sync({ force: true });
+    await syncSeed();
+
+});
+
+
+describe('Testing the /musicians endpoint', () => {
+test('Testing the /musicians endpoint with seeded data', async () => {
     const response = await request(app).get('/musicians');
     const responseData = JSON.parse(response.text);
     expect(response.statusCode).toBe(200);
-    expect(responseData.length).toBe(3);
-})
-test(' Testing the /musicians/:id endpoint', async () => {
+    expect(responseData.length).toBeGreaterThan(0);
+});
+test('Testing the /musicians/:id endpoint with seeded data', async () => {
     const response = await request(app).get('/musicians/1');
     const responseData = JSON.parse(response.text);
     expect(response.statusCode).toBe(200);
     expect(responseData.id).toBe(1);
-})
-test(' Testing the /musicians POST endpoint', async () => {
+});
+
+test('Testing the /musicians POST endpoint', async () => {
     const response = await request(app)
-    .post('/musicians')
-    .send({
-        name: 'Calvin',
-        instrument: 'Guitar'
-    });
+        .post('/musicians')
+        .send({
+            name: 'Calvin',
+            instrument: 'Guitar'
+        });
     const responseData = JSON.parse(response.text);
     expect(response.statusCode).toBe(200);
     expect(responseData.name).toBe('Calvin');
     expect(responseData.instrument).toBe('Guitar');
-})
-test(' Testing the /musicians PUT endpoint', async () => {
+});
+
+test('Testing the /musicians PUT endpoint', async () => {
     const response = await request(app)
-    .put('/musicians/1')
-    .send({
-        name: 'Calvin',
-        instrument: 'Guitar'
-    });
+        .put('/musicians/1')
+        .send({
+            name: 'Adrian',
+            instrument: 'Piano'
+        });
     const responseData = JSON.parse(response.text);
     expect(response.statusCode).toBe(200);
-    expect(responseData.name).toBe('Calvin');
-    expect(responseData.instrument).toBe('Guitar');
-})
-test(' Testing the /musicians DELETE endpoint', async () => {
+    expect(responseData.name).toBe('Adrian');
+    expect(responseData.instrument).toBe('Piano');
+});
+
+test('Testing the /musicians DELETE endpoint', async () => {
     const response = await request(app).delete('/musicians/1');
     const responseData = JSON.parse(response.text);
     expect(response.statusCode).toBe(200);
     expect(responseData.message).toBe('Musician 1 deleted');
-})
+});
 
-
-
+    
+afterAll(async () => {
+    await db.close();
+});
     
 })
--- src/app.js
+++ src/app.js
@@ -1,40 +1,16 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
-
+const  Musician  = require("../models/index")
+const  db  = require("../db/connection")
+const router = require('../routes/musicians');
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
-app.get("/musicians", async (req, res) => {
-    const musicians = await Musician.findAll();
-    res.json(musicians);
-});
-
-app.get("/musicians/:id", async (req, res) => {
-    const musician = await Musician.findByPk(req.params.id);
-    res.json(musician);
-})
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.post("/musicians", async (req, res) => {
-    const musician = await Musician.create(req.body);
-    res.json(musician);
-})
-app.put("/musicians/:id", async (req, res) => {
-    const musician = await Musician.findByPk(req.params.id);
-    await musician.update(req.body);
-    res.json(musician);
-})
-app.delete("/musicians/:id", async (req, res) => {
-    const musician = await Musician.findByPk(req.params.id);
-    await musician.destroy();
-    res.json({ message: `Musician ${req.params.id} deleted` });
-})
-
-
-
+app.use('/musicians', router);
+// app.get('/', (req, res) => {
+//     res.send('Server is running');
+// });
 
 
 module.exports = app;
